22cb078fce3ebbdd6c896db4c7efba920ac1732b
Eduardo Kenzo Rocha Ychibassi <eychibassi@gmail.com>
2026-03-05 16:09:04 -0300

    style:fixa o header no topo e remove a linha de divisão

--- scr/components/Header.tsx
+++ scr/components/Header.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 export const Header = () => {
   return (
-  <header className="flex items-center justify-between py-8 px-10 mb-8 border-b-2 border-blue-600 bg-white">
+  <header className="sticky top-0 z-50 flex items-center justify-between py-8 px-10 mb-8 shadow-[0_10px_25px_-5px_rgba(0,0,0,0.2),0_8px_10px_-6px_rgba(0,0,0,0.2)] border-b border-slate-200 bg-white">
   {/* LADO ESQUERDO: iRepair */}
   <div className="text-5xl tracking-tighter ml-0 font-bold flex items-center leading-[0.8]"> 
     <span className="text-blue-600">i</span>
@@ -10,7 +10,7 @@ export const Header = () => {
   </div>
 
   
-  {/* LADO DIREITO: Painel e Perfil com ajuste manual de altura */}
+  {/* LADO DIREITO: Painel e Perfil */}
   <div className="flex items-center gap-12 mr-4 translate-y-1">
     {/* Indicador de Localização */}
     <div className="flex items-center gap-3">
